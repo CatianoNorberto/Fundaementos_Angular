00c6128a1841edc6722a636c0b552b54cace6f69
CatianoNorberto <catianonorberto@gmail.com>
2024-04-12 15:57:39 -0300

    criação do newslatter do formulario

---
+++ src/app/components/newslatter-form/newslatter-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newslatter-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './newslatter-form.component.html',
+  styleUrl: './newslatter-form.component.scss'
+})
+export class NewslatterFormComponent {
+
+}
