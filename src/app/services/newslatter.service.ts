d5ae96555272e5e80d7d14289ec0785269ebddf2
CatianoNorberto <catianonorberto@gmail.com>
2024-04-15 11:07:59 -0300

    O consumo de dados com a api feito com sucesso

--- src/app/services/newslatter.service.ts
+++ src/app/services/newslatter.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewslatterService {
+  private endpointUrl = "https://https://faed47pcwb7biktidlecuafuty0aegep.lambda-url.us-east-1.on.aws/";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  senData(name: string, email: string) : Observable<NewslatterMessage>{
+    const data = { name, email };
+
+    return this.http.post<NewslatterMessage>(this.endpointUrl, data)
+  }
 }
