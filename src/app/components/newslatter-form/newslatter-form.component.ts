8ef43aff026b9db050d1dc7c2e7f90bfe9d1ab2b
CatianoNorberto <catianonorberto@gmail.com>
2024-04-15 10:42:56 -0300

    validação do formulario

--- src/app/components/newslatter-form/newslatter-form.component.ts
+++ src/app/components/newslatter-form/newslatter-form.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'newslatter-form',
   standalone: true,
-  imports: [BtnPrimaryComponent],
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
   templateUrl: './newslatter-form.component.html',
   styleUrl: './newslatter-form.component.scss'
 })
 export class NewslatterFormComponent {
+  newsletterForm!: FormGroup;
 
+  constructor() {
+    this.newsletterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.newsletterForm.value)
+  }
 }
